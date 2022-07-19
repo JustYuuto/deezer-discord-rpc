b412d3aac334ac9b2b5edccca12c2493b6478c54
NetherMC <thelio.trinite@outlook.com>
2022-07-19 13:23:07 +0200

    added a window + a notification when the window is closed

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import * as RPC from 'discord-rpc';
 import { CLIENT_ID, ACTIVITY_REFRESH_EVERY } from './variables';
-import { initTrayIcon, setActivity } from './functions';
-import { app } from 'electron';
+import { initTrayIcon, setActivity, loadWindow } from './functions';
+import { app, BrowserWindow, Notification } from 'electron';
 
 const client = new RPC.Client({
   transport: 'ipc'
 });
 
+app.commandLine.appendSwitch('disable-site-isolation-trials');
+
 app.whenReady().then(() => {
   initTrayIcon(app);
+  loadWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) loadWindow();
+  });
 });
+
+app.on('window-all-closed', () => {
+  const notification = new Notification({
+    title: 'Deezer Discord RPC',
+    body: 'The RPC launched in background mode. Click on the tray icon to show the window.'
+  });
+  notification.show();
+})
 
 client.on('ready', () => {
   console.log([
