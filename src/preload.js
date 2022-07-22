a312a80d321fe68620fbcd3774dbe8a1ba0150a2
NetherMC <thelio.trinite@outlook.com>
2022-07-22 19:39:33 +0200

    preload script

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,12 @@
-const alertText = 
-`READ THE TEXT BELOW BEFORE CLOSING THIS WINDOW!!
+if (localStorage.getItem('first_run') !== false) {
+  const alertText = 
+  `PLEASE READ THE TEXT BELOW BEFORE CLOSING THIS WINDOW!!
 
-A deezer login page will open. Login to your account, then it will redirecting to Deezer. You can close the window next after your login (it will run in background).
+  A deezer login page will open. Login to your account, then it will redirecting to Deezer. The window is not closable due to a bug that makes the RPC not working at all.
 
-The web player will automatically update the current song, so don't worry the RPC it will update by itself.`;
+  The web player will automatically update the current song, so don't worry the RPC it will update by itself.`;
 
-alert(alertText);
+  alert(alertText);
+
+  localStorage.setItem('first_run', false);
+}
