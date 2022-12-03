1797814cba8c1bd284db5f012772ded56f4da89a
NetherMC <thelio.trinite@outlook.com>
2022-12-03 15:31:54 +0100

    preload script

---
+++ src/preload.js
@@ -0,0 +1,7 @@
+const alertText =
+`PLEASE READ THE TEXT BELOW IF YOU DIDN'T READ IT BEFORE CLOSING THIS WINDOW!!
+
+A Deezer login page will open. Login to your account, then it will redirecting to Deezer. After that you can close the window, it will run in background.
+To quit the RPC use the tray icon.`;
+
+alert(alertText);
