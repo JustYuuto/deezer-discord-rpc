8b6e38838b4b37a35d15ed3c76c2968d69c2e49b
Yuuto <thelio.trinite@outlook.com>
2023-02-24 20:13:54 +0100

    added user agent variable

--- src/variables.ts
+++ src/variables.ts
@@ -1,3 +1,4 @@
 export const deezerApiBase = 'https://api.deezer.com';
 export const clientId = '899229435130183690';
 export const useAsMainApp = true;
+export const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
