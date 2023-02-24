3ca58ec55d548926e582a8ac84680f798da4ce7c
Yuuto <thelio.trinite@outlook.com>
2023-02-24 20:09:13 +0100

    rpc on ready

--- src/index.ts
+++ src/index.ts
@@ -19,59 +19,8 @@ app.whenReady().then(async () => {
   });
 });
 
-client.on('ready', () => {
-  console.log([
-    `Deezer Discord RPC version ${require('../package.json').version}`,
-    `Authed for user ${client.user?.username}#${client.user?.discriminator}`
-  ].join('\n'));
-
-  win.webContents.on('did-finish-load', () => {
-    wait(5000).then(() => {
-      let currentTrack;
-      setInterval(() => {
-        let code =
-          `(() => {
-            const albumId = document.querySelector('.track-link[href*="album"]')?.getAttribute('href').split('/')[3];
-            const trackName = document.querySelector('.track-link[href*="album"]')?.textContent;
-            const playing = !!document.querySelector('#page_player > div > div.player-controls > ul > li:nth-child(3) > button > svg[data-testid="PauseIcon"]');
-            const songTime = document.querySelector('#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-counter-max')?.textContent?.split(':');
-            const timeLeft = document.querySelector('#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-counter-current')?.textContent?.split(':');
-            return JSON.stringify({ albumId, trackName, playing, songTime: { minutes: songTime[0], seconds: songTime[1] }, timeLeft: { minutes: timeLeft[0], seconds: timeLeft[1] } });
-          })();`;
-        win.webContents.executeJavaScript(code, true).then(async (result) => {
-          result = JSON.parse(result);
-          const songTime = Date.now() + (((+0) * 60 * 60 + (+result.songTime.minutes) * 60 + (+result.songTime.seconds)) * 1000);
-          const timeLeft = songTime - (((+0) * 60 * 60 + (+result.timeLeft.minutes) * 60 + (+result.timeLeft.seconds)) * 1000);
-          if (currentTrack?.title !== result.trackName) {
-            const trackId = await findTrackInAlbum(result.trackName, result.albumId);
-            const track = await getTrack(trackId);
-            const album = await getAlbum(result.albumId);
-            currentTrack = {
-              trackId, trackTitle: track.title, trackArtists: track.contributors?.map(c => c.name)?.join(', '), trackLink: track.link,
-              albumCover: album.cover_medium, albumTitle: album.title, app
-            };
-            await setActivity({
-              client, albumId: result.albumId, playing: result.playing, timeLeft, app, ...currentTrack
-            });
-          } else {
-            if (!currentTrack) {
-              const trackId = await findTrackInAlbum(result.trackName, result.albumId);
-              const track = await getTrack(trackId);
-              const album = await getAlbum(result.albumId);
-              currentTrack = {
-                trackId, trackTitle: track.title, trackArtists: track.contributors?.map(c => c.name)?.join(', '), trackLink: track.link,
-                albumCover: album.cover_medium, albumTitle: album.title, app
-              };
-            }
-            await setActivity({
-              client, albumId: result.albumId, playing: result.playing, timeLeft, app, ...currentTrack
-            });
-          }
-          currentTrack.title = result.trackName;
-        });
-      }, 500);
-    });
-  });
+rpcClient.on('ready', () => {
+  console.log(`[RPC] Authed for user ${rpcClient.user?.username}#${rpcClient.user?.discriminator}`);
 });
 
 process.on('beforeExit', () => {
