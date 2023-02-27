60c408c8fb4944eeaf782118f8aa16ce375b77c1
Yuuto <thelio.trinite@outlook.com>
2023-02-27 20:35:07 +0100

    artists separator

--- src/variables.ts
+++ src/variables.ts
@@ -5,3 +5,4 @@ export const useAsMainApp = true;
 export const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
 export const protocol = 'deezer-discord-rpc';
 export const spotifyRedirectUri = `${protocol}://spotify-callback/`;
+export const artistsSeparator = ', ';
