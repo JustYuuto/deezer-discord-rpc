400e9b55b41d80d9c0d2e4f290154de44604f5bf
Yuuto <thelio.trinite@outlook.com>
2023-02-27 19:37:41 +0100

    user agent

--- src/variables.ts
+++ src/variables.ts
@@ -1,6 +1,7 @@
 export const deezerApiBase = 'https://api.deezer.com';
 export const clientId = '899229435130183690';
 export const useAsMainApp = true;
+// Windows 10 (x64) with Google Chrome 110.0.0.0
 export const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
 export const protocol = 'deezer-discord-rpc';
 export const spotifyRedirectUri = `${protocol}://spotify-callback/`;
