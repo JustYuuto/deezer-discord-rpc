c28d41c809a0379ac301e7f8679b1d457f2937b0
Yuuto <thelio.trinite@outlook.com>
2023-02-27 19:37:13 +0100

    updater

---
+++ src/utils/Updater.ts
@@ -0,0 +1,51 @@
+import { log } from './Log';
+import axios from 'axios';
+import { version } from '../../package.json';
+import { dialog, shell } from 'electron';
+
+export default function updater(fromStartup: boolean = false) {
+  log('Updater', 'Checking for updates...');
+  return getLatestRelease()
+    .then(release => {
+      if (release.tag_name !== version) {
+        log('Updater', 'The version', release.tag_name, 'is available to download!');
+        dialog.showMessageBox({
+          type: 'info',
+          title: 'Update available',
+          buttons: ['Cancel', 'Download'],
+          message: `The version ${release.tag_name} is available to download!`,
+          defaultId: 0,
+        }).then(({ response }) => {
+          if (response === 1) {
+            shell.openExternal(release.assets.find(f => f.name.split('.').pop() === 'exe').browser_download_url);
+          }
+        });
+      } else {
+        log('Updater', 'No updates found.');
+        if (!fromStartup)
+          dialog.showMessageBox(null, {
+            type: 'info',
+            title: 'No update available',
+            message: 'You are using the latest version.',
+          });
+      }
+    })
+    .catch(reason => {
+      log('Updater', 'Cannot get the latest release:', reason?.toString());
+      dialog.showMessageBox(null, {
+        type: 'error',
+        buttons: ['Close', 'Retry'],
+        title: 'Cannot get latest release',
+        message: 'Cannot get the latest release.',
+        detail: reason?.toString(),
+      }).then(({ response }) => {
+        if (response === 1) updater();
+      });
+    });
+}
+
+export async function getLatestRelease() {
+  const url = 'https://api.github.com/repos/JustYuuto/deezer-discord-rpc/releases/latest';
+  const release = await axios.get(url);
+  return release.data;
+}
