dcf55193bae95f671b6e23f9548946467c93fa09
Yuuto <thelio.trinite@outlook.com>
2023-03-15 16:49:17 +0100

    user agents

--- src/variables.ts
+++ src/variables.ts
@@ -1,15 +1,16 @@
 export const deezerApiBase = 'https://api.deezer.com';
 export const clientId = '899229435130183690';
 export const useAsMainApp = true;
-// Windows 10 (x64) with Google Chrome 110.0.0.0
-export const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+export const userAgents = {
+  deezerApp: `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36`,
+  discordApp: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9011 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36'
+}
 export const protocol = 'deezer-discord-rpc';
 export const spotifyRedirectUri = `${protocol}://spotify-callback/`;
 export const artistsSeparator = ', ';
 export const noWsActivity = {
   op: 3,
   d: {
-    status: 'online',
     since: 0,
     afk: false,
     activities: []
