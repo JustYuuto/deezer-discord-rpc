17cd71de6bd4acad0a5314554230246e9b19ce46
JustYuuto <thelio.trinite@outlook.com>
2023-04-03 17:35:47 +0200

    build

--- bin/build.js
+++ bin/build.js
@@ -1,19 +1,24 @@
 #!/bin/env /usr/bin/node
 
 const { existsSync, rmSync, copyFileSync } = require('fs');
+const { copySync } = require('fs-extra');
 const { resolve } = require('path');
 const { execSync } = require('child_process');
+const builder = require('electron-builder');
 
+// If the TypeScript build folder exists, we need to delete it
 if (existsSync(resolve('build'))) {
   rmSync(resolve('build'), { recursive: true });
   console.log('Removed "build" directory');
 }
 
+// If the Electron app build folder exists, we need to delete it
 if (existsSync(resolve('dist'))) {
   rmSync(resolve('dist'), { recursive: true });
   console.log('Removed "dist" directory');
 }
 
+// TypeScript Compilation
 console.log('Compiling TypeScript...');
 try {
   execSync('npx tsc --resolveJsonModule', { stdio: 'inherit' });
@@ -24,12 +29,35 @@ try {
 
 copyFileSync(resolve('src', 'prompt.html'), resolve('build', 'src', 'prompt.html'));
 
+// Icon
 console.log('Compiling icon to an ICO file...');
 execSync('yarn run icon');
 console.log('Compiled icon to an ICO file');
+copySync(resolve('src', 'img'), resolve('build', 'src', 'img'));
 
+// App build
 console.log('Building EXE...');
-execSync('npx electron-builder', { stdio: 'inherit' });
-
-console.log('');
-console.log(`EXE built! The file can be found at "${resolve('dist', `Deezer Discord RPC Setup ${require('../package.json').version}.exe`)}"`);
+const config = {
+  appId: 'com.github.yuuto.deezerdiscordrpc',
+  productName: 'Deezer Discord RPC',
+  mac: {
+    category: 'public.app-category.music',
+    target: 'dmg'
+  },
+  protocols: [
+    {
+      name: 'deezer-discord-rpc',
+      schemes: ['deezer-discord-rpc']
+    }
+  ],
+  files: [
+    '!src/*',
+    '!src/**/*',
+    'build/package.json',
+    'build/src/*',
+    'build/src/**/*'
+  ]
+};
+builder.build({ targets: builder.Platform.WINDOWS, config }).then(() => {
+  console.log(`\nEXE built!`);
+});
