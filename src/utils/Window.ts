c1e4af9b41a4c1d78ef7cac367cf4afe2b7a244a
JustYuuto <thelio.trinite@outlook.com>
2023-04-09 13:15:06 +0200

    activity

--- src/utils/Window.ts
+++ src/utils/Window.ts
@@ -13,6 +13,7 @@ import { BrowserWindow, ipcMain, Menu, shell } from 'electron';
 import { setActivity }                         from './Activity';
 
 export let win: BrowserWindow;
+let currentTrack;
 
 export async function load(app: Electron.App) {
   win = new BrowserWindow({
@@ -78,59 +79,63 @@ export async function load(app: Electron.App) {
   });
 
   wait(5000).then(() => {
-    let currentTrack;
-    setInterval(() => {
-      const client = (Config.get(app, 'use_listening_to') ? DiscordWebSocket : RPC).client;
-      let code =
-        `(() => {
-          const albumId = document.querySelector('.track-link[href*="album"]')?.getAttribute('href').split('/')[3];
-          const trackName = document.querySelector('.track-link[href*="album"]')?.textContent;
-          const playing = !!document.querySelector('#page_player > div > div.player-controls > ul > li:nth-child(3) > button > svg[data-testid="PauseIcon"]');
-          const songTime = document.querySelector('#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-counter-max')?.textContent?.split(':');
-          const timeLeft = document.querySelector('#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-counter-current')?.textContent?.split(':');
-          return JSON.stringify({ albumId, trackName, playing, songTime: { minutes: songTime[0], seconds: songTime[1] }, timeLeft: { minutes: timeLeft[0], seconds: timeLeft[1] } });
-        })();`;
-      win.webContents.executeJavaScript(code, true).then(async (result) => {
-        result = JSON.parse(result);
-        const lengthFormat = (minutes: string, seconds: string): number => ((+0) * 60 * 60 + (+minutes) * 60 + (+seconds)) * 1000;
-        const songTime = Date.now() + lengthFormat(result.songTime.minutes, result.songTime.seconds);
-        const timeLeft = songTime - lengthFormat(result.timeLeft.minutes, result.timeLeft.seconds);
-        if (currentTrack?.trackTitle !== result.trackName || currentTrack?.playing !== result.playing) {
-          let reason;
-          if (currentTrack?.trackTitle !== result.trackName) reason = `music got changed`;
-          else if (currentTrack?.playing !== result.playing) reason = `music got ${result.playing ? 'played' : 'paused'}`;
-          log('Activity', `Updating because ${reason}`);
-          const trackId = await findTrackInAlbum(result.trackName, result.albumId);
-          const track = await getTrack(trackId);
-          const album = await getAlbum(result.albumId);
-          currentTrack = {
-            trackId,
-            trackTitle: track.title,
-            trackArtists: track.contributors?.map(c => c.name)?.join(artistsSeparator),
-            trackLink: track.link,
-            albumCover: Config.get(app, 'use_listening_to') ?
-              await Spotify.getCover({
-                albumTitle: album.title, title: track.title, artists: track.contributors?.map(c => c.name)?.join(', ')
-              }, app).catch(async () => {
-                log('Spotify Covers', 'Access token expired; refreshing it');
-                await Spotify.accessToken(Config.get(app, 'spotify_refresh_token')).then((res) => {
-                  const { access_token, expires_in, token_type } = res.data;
-                  Config.set(app, 'spotify_access_token', access_token);
-                  Config.set(app, 'spotify_expires_at', Date.now() + expires_in);
-                  Config.set(app, 'spotify_token_type', token_type);
-                });
-              }) :
-              album.cover_medium,
-            albumTitle: album.title,
-            playing: result.playing
-          };
-          await setActivity({
-            client, albumId: result.albumId, timeLeft, app, ...currentTrack, songTime
-          }).then(() => log('Activity', 'Updated'));
-        }
-        currentTrack.trackTitle = result.trackName;
-        currentTrack.playing = result.playing;
-      });
-    }, 1000);
+    runJs(win, `document.querySelector('.slider-track-input.mousetrap').addEventListener('click', () => ipcRenderer.send('update_activity', 'update_activity'))`);
+    ipcMain.on('update_activity', () => updateActivity(app, true));
+    setInterval(() => updateActivity(app), 1000);
+  });
+}
+
+async function updateActivity(app: Electron.App, currentTimeChanged?: boolean) {
+  const client = (Config.get(app, 'use_listening_to') ? DiscordWebSocket : RPC).client;
+  let code =
+    `(() => {
+      const albumId = document.querySelector('.track-link[href*="album"]')?.getAttribute('href').split('/')[3];
+      const trackName = document.querySelector('.track-link[href*="album"]')?.textContent;
+      const playing = !!document.querySelector('#page_player > div > div.player-controls > ul > li:nth-child(3) > button > svg[data-testid="PauseIcon"]');
+      const songTime = document.querySelector('#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-counter-max')?.textContent?.split(':');
+      const timeLeft = document.querySelector('#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-counter-current')?.textContent?.split(':');
+      return JSON.stringify({ albumId, trackName, playing, songTime: { minutes: songTime[0], seconds: songTime[1] }, timeLeft: { minutes: timeLeft[0], seconds: timeLeft[1] } });
+    })();`;
+  runJs(win, code, true).then(async (result) => {
+    result = JSON.parse(result);
+    const lengthFormat = (minutes: string, seconds: string): number => ((+0) * 60 * 60 + (+minutes) * 60 + (+seconds)) * 1000;
+    const songTime = Date.now() + lengthFormat(result.songTime.minutes, result.songTime.seconds);
+    const timeLeft = songTime - lengthFormat(result.timeLeft.minutes, result.timeLeft.seconds);
+    if (currentTrack?.trackTitle !== result.trackName || currentTrack?.playing !== result.playing || currentTimeChanged === true) {
+      let reason;
+      if (currentTrack?.trackTitle !== result.trackName) reason = 'music got changed';
+      else if (currentTrack?.playing !== result.playing) reason = `music got ${result.playing ? 'played' : 'paused'}`;
+      else if (currentTimeChanged && currentTimeChanged === true) reason = 'current song time changed';
+      log('Activity', 'Updating because', reason);
+      const trackId = await findTrackInAlbum(result.trackName, result.albumId);
+      const track = await getTrack(trackId);
+      const album = await getAlbum(result.albumId);
+      currentTrack = {
+        trackId,
+        trackTitle: track.title,
+        trackArtists: track.contributors?.map(c => c.name)?.join(artistsSeparator),
+        trackLink: track.link,
+        albumCover: Config.get(app, 'use_listening_to') ?
+          await Spotify.getCover({
+            albumTitle: album.title, title: track.title, artists: track.contributors?.map(c => c.name)?.join(', ')
+          }, app).catch(async () => {
+            log('Spotify Covers', 'Access token expired; refreshing it');
+            await Spotify.accessToken(Config.get(app, 'spotify_refresh_token')).then((res) => {
+              const { access_token, expires_in, token_type } = res.data;
+              Config.set(app, 'spotify_access_token', access_token);
+              Config.set(app, 'spotify_expires_at', Date.now() + expires_in);
+              Config.set(app, 'spotify_token_type', token_type);
+            });
+          }) :
+          album.cover_medium,
+        albumTitle: album.title,
+        playing: result.playing
+      };
+      await setActivity({
+        client, albumId: result.albumId, timeLeft, app, ...currentTrack, songTime
+      }).then(() => log('Activity', 'Updated'));
+    }
+    currentTrack.trackTitle = result.trackName;
+    currentTrack.playing = result.playing;
   });
 }
