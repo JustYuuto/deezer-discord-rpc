ee240ed41ff195d477fc12d1f3b6fd0e7966a2c5
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-05-01 00:22:09 -0400

    websocket

--- src/utils/DeezerWebSocket.ts
+++ src/utils/DeezerWebSocket.ts
@@ -1,4 +1,9 @@
 import { WebSocketServer, WebSocket } from 'ws';
+import { log } from './Log';
+import { runJs } from "../functions";
+import { win } from "./Window";
+import { findTrackInAlbum, getAlbum } from "../activity/album";
+import { getTrack } from "../activity/track";
 
 export let server: WebSocket;
 export const events = {
@@ -10,18 +15,56 @@ export function start() {
     port: 5432,
   });
 
+  socket.on('listening', () => {
+    log('Local WS', 'Started server');
+  });
+
   socket.on('connection', (ws) => {
+    log('Local WS', `New connection detected`);
     server = ws;
     ws.on('error', console.error);
 
     ws.on('message', (rawData) => {
       const payload = JSON.parse(rawData.toString());
-      const { type } = payload;
+      const { type, event }: { type: 'ping' | 'message', event: Events } = payload;
 
       switch (type) {
         case 'ping':
           ws.send(JSON.stringify({ type: 'pong' })); break;
       }
+
+      switch (event) {
+        case 'CURRENT_TRACK':
+          let code =
+            `(() => {
+              const albumId = document.querySelector('.track-link[href*="album"]')?.getAttribute('href').split('/')[3];
+              const trackName = document.querySelector('.track-link[href*="album"]')?.textContent;
+              const playing = !!document.querySelector('#page_player > div > div.player-controls > ul > li:nth-child(3) > button > svg[data-testid="PauseIcon"]');
+              const position = parseInt(document.querySelector('input.slider-track-input.mousetrap').getAttribute('value')) * 1000;
+              return JSON.stringify({ albumId, trackName, playing, position });
+            })();`;
+          runJs(win, code).then(async (result) => {
+            result = JSON.parse(result);
+            const trackId = await findTrackInAlbum(result.trackName, result.albumId);
+            const track = await getTrack(trackId);
+            const album = await getAlbum(result.albumId);
+            ws.send(JSON.stringify({
+              type: 'message',
+              event: 'CURRENT_TRACK',
+              data: {
+                track, album, playing: result.playing, position: result.position
+              }
+            }));
+          });
+          break;
+        case 'SET_PLAYING':
+          runJs(win, 'document.querySelector(\'.player-controls > .svg-icon-group > .svg-icon-group-item:nth-child(3) > button\')?.click()');
+          break;
+        case 'NEXT':
+
+      }
     });
   });
 }
+
+declare type Events = 'CURRENT_TRACK' | 'SET_PLAYING' | 'NEXT';
