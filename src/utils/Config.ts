aa5bc0851f0a14eb804746a4c346525b72837929
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-10 01:45:26 +0200

    config

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,12 +1,24 @@
 import { join } from 'path';
 import { existsSync, writeFileSync } from 'fs';
+import { dialog } from 'electron';
 
 export function set(app: Electron.App, key: string, value: any) {
   const path = getConfigPath(app);
   if (!existsSync(path)) writeFileSync(path, '{}');
   const data = require(path);
   data[key] = value;
-  writeFileSync(path, JSON.stringify(data));
+  try {
+    writeFileSync(path, JSON.stringify(data));
+  } catch (e) {
+    dialog.showMessageBox(null, {
+      type: 'error',
+      buttons: ['Close'],
+      title: 'Failed to write config file',
+      message: `An error occurred while writing to ${path}`,
+      detail: e?.toString(),
+      defaultId: 0
+    });
+  }
 }
 
 export function get(app: Electron.App, key?: string) {
