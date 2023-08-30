37d49255c119543e971aeeff96fa14543b9c576c
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-30 15:15:06 +0200

    variables

--- src/variables.ts
+++ src/variables.ts
@@ -1,7 +1,4 @@
-export const deezerApiBase = 'https://api.deezer.com';
 export const clientId = '899229435130183690';
 export const userAgents = {
   deezerApp: `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36`,
-  discordApp: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.60 Chrome/108.0.5359.215 Electron/22.3.2 Safari/537.36'
-}
-export const artistsSeparator = ', ';
+};
