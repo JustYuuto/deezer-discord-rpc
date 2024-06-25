19a6a732cb6b13e1c69fde9076681d5a51479d02
NamelessNanashi <johnnyjel@pm.me>
2024-06-24 21:12:59 -0500

    remove react as it cases building to fail (manifest v3)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, session } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import { log } from './utils/Log';
 import * as Config from './utils/Config';
 import * as Tray from './utils/Tray';
@@ -6,7 +6,6 @@ import updater from './utils/Updater';
 import * as DiscordWebSocket from './utils/DiscordWebSocket';
 import * as RPC from './utils/RPC';
 import * as Window from './utils/Window';
-import { join } from 'path';
 import { version } from '../package.json';
 import {showWindow} from './utils/Window';
 
@@ -26,9 +25,6 @@ app.whenReady().then(async () => {
   }
 
   await Tray.init(app, RPC.client);
-  if (process.argv0.includes('node')) {
-    await session.defaultSession.loadExtension(join(process.cwd(), 'src', 'react-devtools'));
-  }
   await Window.load(app);
   await updater(true);
 
